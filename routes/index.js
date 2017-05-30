646ccef2560fa0661ad15d7fd5c8a0226d4fc7bd
rosa1995 <sandorosa1@gmail.com>
2017-05-29 19:06:15 -0600

    Añadir mensaje y Json

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mensaje', function(req,res,next){
+  res.render('mensaje',{"txtMensaje":"","msg":""});
+});
+
+var mensajes = [];
+
+router.post('/mensaje',function(req,res,next){
+  console.log(req.body);
+    mensajes.push(req.body.txtMensaje);
+    var msgs = mensajes;
+    var rtObject = {};
+    rtObject.txtMensaje = req.body.txtMensaje;
+    rtObject.msg= msgs;
+  res.render('mensaje', rtObject);
+});
+
+
+router.get('/api/datos', function(req,res,next){
+  var datos = {"Nombre":"Rosa","Apellido":"Sandoval","direccion":"San ignacio F:M","correo":"sandorosa1@gmail.com"};
+  res.json(datos);
+
+});
+
 module.exports = router;
